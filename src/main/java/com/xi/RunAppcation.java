c03be156b510c9d898d29f38c5890d4c1f33cabd
liuxi <469541935@qq.com>
2021-03-21 23:31:49 +0800

    test

---
+++ src/main/java/com/xi/RunAppcation.java
@@ -0,0 +1,11 @@
+package com.xi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunAppcation {
+    public static void main(String[] args) {
+        SpringApplication.run(RunAppcation.class,args);
+    }
+}
